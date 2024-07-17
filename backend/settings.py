68c10fde5f142bb07927eaeecc66013b3a32cb2c
Eduardo Segantine <eduardo.segantine@gmail.com>
2024-07-17 19:07:17 -0300

    deploy

--- backend/settings.py
+++ backend/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -11,11 +11,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)ib@ocdkthdcr5axv_c-xrfztyejg_&rgd6$0++@=+hv#edw&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['44.211.171.166', 'weather-app-front.s3-website-us-east-1.amazonaws.com',
-                  'localhost', '127.0.0.1', 'ec2-44-212-24-82.compute-1.amazonaws.com']
-
+ALLOWED_HOSTS = [
+    '44.212.24.82',
+    'weather-app-front.s3-website-us-east-1.amazonaws.com',
+    'localhost',
+    '127.0.0.1',
+    'ec2-44-212-24-82.compute-1.amazonaws.com'
+]
 
 # Application definition
 
@@ -67,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -77,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -97,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -109,11 +110,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
